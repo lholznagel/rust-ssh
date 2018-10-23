1227693ed1bf0f4f1574f2f552e6b535fb6828c9
lholznagel <contact@lholznagel.info>
2018-10-23 13:31:45 +0200

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -224,7 +224,12 @@ impl DiffieHellmanKeyExchange {
         let mut padding = ((4 + 1 + payload.len()) % 8) as u8;
         if padding < 4 {
             padding = 8 - padding;
+        } else {
+            padding = 8 + (8 - padding);
         }
+
+        println!("{}" , (4 + 1 + payload.len()));
+        println!("{}", padding);
 
         Builder::new()
             .write_u32(1 + payload.len() as u32 + padding as u32)
