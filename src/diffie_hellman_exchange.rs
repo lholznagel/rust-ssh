35418f42d5a5603d02adcae6f79c62f2b04baa9a
lholznagel <contact@lholznagel.info>
2018-10-25 13:13:41 +0200

    WIP

--- src/diffie_hellman_exchange.rs
+++ src/diffie_hellman_exchange.rs
@@ -49,10 +49,13 @@ impl DiffieHellmanKeyExchange {
 
         // random gen for the curve
         let mut curve_rand = rand::OsRng::new().unwrap();
+        let ed_keypair = ed25519_dalek::Keypair::generate::<Sha512, _>(&mut curve_rand);
         // generate the curve25519 secret
-        let curve_secret = x25519_dalek::generate_secret(&mut curve_rand);
+        //let curve_secret = x25519_dalek::generate_secret(&mut curve_rand);
+        let curve_secret = ed_keypair.secret.to_bytes();
         // generate the curve25519 public
-        let curve_public = x25519_dalek::generate_public(&curve_secret);
+        //let curve_public = x25519_dalek::generate_public(&curve_secret);
+        let curve_public = ed_keypair.public.to_bytes();
         // shared diffie hellman key, combining the own secret with the client public
         let dh = x25519_dalek::diffie_hellman(&curve_secret, &public);
 
@@ -65,34 +68,18 @@ impl DiffieHellmanKeyExchange {
         let host_key = host_key.replace("ssh-ed25519 ", "").replace("\n", "");
         let host_key = base64::decode(&host_key).unwrap();
         let host_key = host_key[(4 + 11)..host_key.len()].to_vec();
+        println!("{:?}", host_key);
 
-        println!("{:?}", self.diffie_hellman.client_kex);
-
-        // hash
-        let mut hasher = Sha256::new(); // H
-        hasher.input(&self.diffie_hellman.client_identifier); // V_C
-        hasher.input(&self.diffie_hellman.server_identifier); // S_C
-        hasher.input(&self.diffie_hellman.client_kex); // I_C
-        hasher.input(&self.diffie_hellman.server_kex); // S_C
-        hasher.input(&host_key.clone()); // K_S
-        hasher.input(&self.e); // e
-        hasher.input(&curve_public.to_bytes()); // f
-        hasher.input(&dh); // K
-
-        // combine the secret and public key to a keypair
-        let mut keys = Vec::new();
-        keys.append(&mut curve_secret.to_vec());
-        keys.append(&mut curve_public.to_bytes().to_vec());
-        let keypair = ed25519_dalek::Keypair::from_bytes(&keys).unwrap();
+        let hash = self.hash(host_key.clone(), curve_public.to_vec(), dh.to_vec());
         // create a signature of H
-        let dh_signed = keypair.sign::<Sha512>(&hasher.result()); // s
+        let dh_signed = self.sign(&ed_keypair, hash).to_bytes().to_vec(); // s
 
         let hash_algo = String::from("ssh-ed25519");
         let h = Builder::new()
             .write_u32(hash_algo.len() as u32)
             .write_vec(hash_algo.as_bytes().to_vec())
-            .write_u32(dh_signed.to_bytes().len() as u32)
-            .write_vec(dh_signed.to_bytes().to_vec())
+            .write_u32(dh_signed.len() as u32)
+            .write_vec(dh_signed.to_vec())
             .build();
 
         let payload = Builder::new()
@@ -103,8 +90,8 @@ impl DiffieHellmanKeyExchange {
             .write_u32(11)
             .write_vec(String::from("ssh-ed25519").as_bytes().to_vec())
             .write_vec(host_key) // K_S
-            .write_u32(curve_public.to_bytes().len() as u32)
-            .write_vec(curve_public.to_bytes().to_vec()) // f
+            .write_u32(curve_public.len() as u32)
+            .write_vec(curve_public.to_vec()) // f
             .write_u32(h.len() as u32)
             .write_vec(h) // s
             .build();
@@ -122,5 +109,58 @@ impl DiffieHellmanKeyExchange {
             .write_vec(payload)
             .write_vec(vec![0; padding as usize])
             .build()
+    }
+
+    pub fn sign(&self, keypair: &ed25519_dalek::Keypair, hash: Vec<u8>) -> ed25519_dalek::Signature {
+        keypair.sign::<Sha512>(&hash)
+    }
+
+    pub fn hash(&self, host_key: Vec<u8>, curve_public: Vec<u8>, dh: Vec<u8>) -> Vec<u8> {
+        let mut hasher = Sha256::new(); // H
+        hasher.input(&self.diffie_hellman.client_identifier); // V_C
+        hasher.input(&self.diffie_hellman.server_identifier); // S_C
+        hasher.input(&self.diffie_hellman.client_kex); // I_C
+        hasher.input(&self.diffie_hellman.server_kex); // S_C
+        hasher.input(&host_key.clone()); // K_S
+        hasher.input(&self.e); // e
+        hasher.input(&curve_public); // f
+        hasher.input(&dh); // K
+        hasher.result().as_slice().to_vec()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_hash() {
+        let their_keypair = vec![114, 81, 155, 115, 26, 23, 71, 238, 73, 129, 40, 214, 225, 137, 42, 54, 77, 147, 175, 134, 1, 17, 239, 115, 40, 116, 40, 77, 29, 248, 237, 43, 172, 53, 97, 87, 51, 161, 9, 197, 122, 37, 107, 85, 80, 23, 155, 33, 145, 227, 109, 56, 126, 133, 204, 130, 172, 106, 80, 122, 132, 159, 12, 67];
+        let their_keypair = ed25519_dalek::Keypair::from_bytes(&their_keypair).unwrap();
+
+        let our_keypair = vec![114, 58, 213, 139, 206, 102, 220, 38, 3, 249, 236, 186, 234, 15, 27, 14, 54, 202, 139, 102, 43, 162, 234, 93, 110, 198, 226, 244, 60, 211, 143, 95, 177, 103, 134, 162, 246, 234, 222, 222, 23, 213, 23, 100, 247, 173, 162, 138, 5, 162, 234, 195, 150, 169, 146, 143, 76, 206, 234, 196, 78, 98, 221, 127];
+        let our_keypair = ed25519_dalek::Keypair::from_bytes(&our_keypair).unwrap();
+
+        let diffie_helmann = DiffiHellman {
+            client_identifier: vec![83, 83, 72, 45, 50, 46, 48, 45, 84, 69, 83, 84, 95, 49],
+            server_identifier: vec![83, 83, 72, 45, 50, 46, 48, 45, 84, 69, 83, 84, 95, 50],
+            client_kex: Vec::new(), // TODO
+            server_kex: Vec::new() // TODO
+        };
+
+        let diffie_hellman_exchange = DiffieHellmanKeyExchange {
+            packet_length: 0,
+            padding_length: 0,
+            ssh_msg_kexdh: 0,
+            e: their_keypair.public.to_bytes().to_vec(),
+            diffie_hellman: diffie_helmann,
+        };
+
+        let host_key = vec![0, 0, 0, 32, 135, 56, 114, 191, 159, 164, 132, 170, 77, 243, 153, 74, 44, 164, 200, 239, 152, 82, 133, 146, 131, 245, 245, 210, 79, 162, 28, 165, 128, 99, 85, 89];
+        let dh = x25519_dalek::diffie_hellman(&our_keypair.secret.to_bytes(), &their_keypair.public.to_bytes());
+        let hash = diffie_hellman_exchange.hash(host_key, our_keypair.public.to_bytes().to_vec(), dh.to_vec());
+        let signed = diffie_hellman_exchange.sign(&our_keypair, hash.clone());
+
+        let _ = our_keypair.verify::<Sha512>(&hash, &signed).unwrap();
     }
 }
