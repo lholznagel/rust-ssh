4df93f8e58695b3153dcd0a677876dcce8b0dbf3
lholznagel <contact@lholznagel.info>
2018-11-07 21:59:01 +0100

    Cleanup WIP

--- src/protocol_version_exchange.rs
+++ src/protocol_version_exchange.rs
@@ -6,7 +6,10 @@ pub struct ProtocolVersionExchange {
 }
 
 impl ProtocolVersionExchange {
-    pub fn parse(data: &[u8; 2048]) -> Result<Self, Error> {
+    pub fn parse(data: &[u8]) -> Result<Self, Error> {
+        if data.len() > 255 {
+            return Err(format_err!("Protocol version too long!"));
+        }
         // check the first three bytes for the String SSH
         // TODO: the first line does not have to be the SSH string
         if data[0] == 83 && data[1] == 83 && data[2] == 72 {
@@ -25,5 +28,32 @@ impl ProtocolVersionExchange {
 
     pub fn build() -> Vec<u8> {
         "SSH-2.0-rssh_0.1.0\r\n".as_bytes().to_vec()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_001() {
+        let data = "SSH-2.0-test-0.1.0\r\n";
+        let result = ProtocolVersionExchange::parse(&data.as_bytes());
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    pub fn test_002() {
+        let data = "SSH-2.0-test-0.1.0 Some_comment\r\n";
+        let result = ProtocolVersionExchange::parse(&data.as_bytes());
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    pub fn test_003() {
+        let mut data = "SSH-2.0-test-0.1.0\r\n".as_bytes().to_vec();
+        data.append(&mut vec![0; 236]);
+        let result = ProtocolVersionExchange::parse(&data);
+        assert!(result.is_err());
     }
 }
