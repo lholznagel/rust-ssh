a47daded33114799703b85b13c48e671397e5f31
Lars Holznagel <contact@lholznagel.info>
2019-02-05 15:09:32 +0100

    Optimized tcp read

--- src/server.rs
+++ src/server.rs
@@ -32,7 +32,7 @@ pub struct SSHServer {
 
 impl SSHServer {
     pub fn new(tcp_listener: TcpStream) -> Self {
-        std::mem::forget(Box::new(42));
+        //std::mem::forget(Box::new(42));
         Self { tcp_listener }
     }
 
@@ -43,15 +43,30 @@ impl SSHServer {
         let mut server_kex = Vec::new();
 
         loop {
-            let mut buffer = [0; 2048];
+            let mut message = Vec::new();
+            let mut buffer = [0; 4];
 
-            // TODO: future: use peek
             let _ = self.tcp_listener.read(&mut buffer).unwrap();
+            if buffer[0] == 83 && buffer[1] == 83 && buffer[2] == 72 {
+                let mut message_buffer = vec![0; 252]; // 256 - the 4 byte already read
+                let _ = self.tcp_listener.read(&mut message_buffer).unwrap();
+                message.append(&mut buffer.to_vec());
+                message.append(&mut message_buffer.to_vec());
+            } else if buffer[0] == 0 && buffer[1] == 0 && buffer[2] == 0 && buffer[3] == 0 {
+                // FIXME
+                std::process::exit(0);
+            } else {
+                let packet_length = unsafe { std::mem::transmute::<[u8; 4], u32>(buffer) }.to_be();
+                let mut message_buffer = vec![0; packet_length as usize];
+                let _ = self.tcp_listener.read_exact(&mut message_buffer).unwrap();
+                message.append(&mut buffer.to_vec());
+                message.append(&mut message_buffer.to_vec());
+            }
 
             match transport.state {
                 State::Version => {
                     // version string will be max 256 chars long
-                    match Version::parse(&buffer[0..256]) {
+                    match Version::parse(&message) {
                         Ok(x) => {
                             transport.client_version = x;
                             transport.server_version = Version::default();
@@ -66,8 +81,8 @@ impl SSHServer {
                     };
                 }
                 State::KeyExchangeInit => {
-                    KexInit::parse(&buffer).unwrap();
-                    match KexInit::parse(&buffer) {
+                    KexInit::parse(&message).unwrap();
+                    match KexInit::parse(&message) {
                         Ok(client_msg_parsed) => {
                             client_kex = client_msg_parsed.build();
                             server_kex = KexInit::default().build();
@@ -82,7 +97,7 @@ impl SSHServer {
                 }
                 State::DiffiHellmanKeyExchange => {
                     match KexDh::parse(
-                        &buffer,
+                        &message,
                         DiffiHellman {
                             client_identifier: transport.client_version.filtered(),
                             server_identifier: transport.server_version.filtered(),
